d55606365d157e1735073ad23463f48d15c996f3
Samuel <samuel.didier-laurent@idweaver.com>
2019-07-09 13:11:16 +0200

    feature(js): simplify boolClass

--- dist/js/main.min.js
+++ dist/js/main.min.js
@@ -12,30 +12,23 @@ const app = new Vue({
         axios.get(url)
             .then((response) => {
                 // handle success
-                this.annotations = response.data.annotations
-                this.columns = Object.keys(this.annotations[0]).filter((column)=> (column !== 'Extra notes' && column !== 'Attribute'))
+                this.annotations = response.data.annotations;
+                this.columns = Object.keys(this.annotations[0]).filter(column => (column !== 'Extra notes' && column !== 'Attribute'));
                 this.filteredAnnotations = this.annotations;
             })
             .catch((error) => {
                 // handle error
-                console.log(error);
+                console.error(error);
             });
     },
     methods:{
         boolClass(value) {
-            let className = '';
-            if (value === 'true') {
-                className = 'true';
-            }
-            if (value === 'false') {
-                className = 'false';
-            }
-            return className;
+            return value;
         },
         filterResults(value) {
-            const annotations = JSON.parse(JSON.stringify(this.annotations))
+            const annotations = JSON.parse(JSON.stringify(this.annotations));
             if (value !== undefined && value !== 'All') {
-                const filteredAnnotations = annotations.filter(item => item['Allowed on'].includes(value))
+                const filteredAnnotations = annotations.filter(item => item['Allowed on'].includes(value));
                 this.filteredAnnotations = filteredAnnotations;
             } else if (value === 'All') {
                 this.filteredAnnotations = annotations;
